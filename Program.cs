bf7d20449959f3365151cb73971dd014d1fc7643
anandacchu <108655411+anandacchu@users.noreply.github.com>
2022-11-03 11:56:08 +0530

    welcome

---
+++ Program.cs
@@ -0,0 +1,10 @@
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Algorithm Program");
+        }
+    }
+}
